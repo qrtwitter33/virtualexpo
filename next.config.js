28068371bc6a34c002d4840dd2defd3f67db692c
qrtwitter33 <qrtwitter33@gmail.com>
2025-08-05 03:45:25 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
